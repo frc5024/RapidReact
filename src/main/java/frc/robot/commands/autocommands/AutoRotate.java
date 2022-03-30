a96479736f122de044d9cdfeb5b1c7adf93391ec
wm-c <wcmeathrel@gmail.com>
2022-03-30 15:30:57 -0400

    minor changes

--- src/main/java/frc/robot/commands/autocommands/AutoRotate.java
+++ src/main/java/frc/robot/commands/autocommands/AutoRotate.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.autocommands;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystem.DriveTrain;
@@ -29,13 +28,6 @@ public class AutoRotate extends CommandBase {
 		turnController.reset();
 
 		turnController.setTolerance(5);
-		// Check if it is faster to turn the other way
-		//double error = goal - driveTrain.getCurrentHeading().getDegrees();
-
-		// if(error > 180){
-		// 	error -= 360;
-		// 	goal *= -1;
-		// }
 
 
 	}
